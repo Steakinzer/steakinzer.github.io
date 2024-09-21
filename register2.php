359a20c901be36ca75392be2aea29f225ae02c19
Clément Jodin <117357226+Steakinzer@users.noreply.github.com>
2024-09-21 12:52:53 +0200

    Add files via upload

---
+++ register2.php
@@ -0,0 +1,17 @@
+var com_adswizz_register_PROTOCOL_VERSION = '2.2.0';
+
+if (typeof com_adswizz_synchro_listenerid === 'undefined') {
+    var com_adswizz_synchro_listenerid = '996aefd92b93060a18e6635f5fe7e611';
+}
+else {
+    com_adswizz_synchro_listenerid = '996aefd92b93060a18e6635f5fe7e611';
+}
+
+if (typeof com_adswizz_synchro_listnerid === 'undefined') {
+    var com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+else {
+    com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+var aw_0_req_gdpr = false;
+var us_privacy = '';
